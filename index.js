cab9f957ce2a2a8c0b388cce48f8841fce173ff1
Hugo Gomes <gomes.hugo00@gmail.com>
2022-09-02 15:43:17 -0300

    Conexão com o banco de dados

--- index.js
+++ index.js
@@ -1,10 +1,41 @@
-import express, {json} from "express";
+import express, { json } from "express";
 import cors from "cors";
-const PORT = 5000;
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+
 const server = express();
 
+dotenv.config();
 server.use(json());
 server.use(cors());
+
+const PORT = process.env.PORT;
+const DB_HOST = process.env.MONGO_URI;
+
+
+const banco = new MongoClient(DB_HOST);
+let db;
+
+banco.connect().then(() => {
+	db = banco.db("uol");
+});
+
+
+
+server.get("/", async (req, res) => {
+	
+	const data = await db.collection("uol").find().toArray();
+	res.send(data);
+});
+
+server.post("/", (req, res) => {
+
+	const { nome } = req.body;
+
+	db.collection("uol").insertOne({nome: nome}).then( () => {
+		res.status(201).send("Usuário inserido com sucesso!");
+	});
+});
 
 server.listen(PORT, () => {
 	console.log(`Servidor rodando na porta ${PORT}`);
